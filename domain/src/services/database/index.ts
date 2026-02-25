0a888685a572fb19014ceb250c55a81e16d1a65b
John Prutton <johnrprutton@gmail.com>
2026-02-25 23:21:47 +0200

    [Feature:domain] Model database error

--- domain/src/services/database/index.ts
+++ domain/src/services/database/index.ts
@@ -1,9 +1,17 @@
+import * as Data from "effect/Data"
 import * as Effect from "effect/Effect"
 import * as ServiceMap from "effect/ServiceMap"
+
+export class DatabaseError extends Data.TaggedError("DatabaseError")<{
+	message: string
+	error: string
+}> {}
+
+type DatabaseQuery<T> = Effect.Effect<T, DatabaseError>
 
 export class Database extends ServiceMap.Service<
 	Database,
 	{
-		readonly healthCheck: Effect.Effect<true>
+		readonly healthCheck: DatabaseQuery<true>
 	}
 >()("Database") {}
