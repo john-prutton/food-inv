a0ae009c39fd659989485b5cf71a37d8730474bd
John Prutton <johnrprutton@gmail.com>
2026-02-27 00:56:40 +0200

    [Refactor:database] Construct Db dependencies in its layer

--- core/database/src/db/index.ts
+++ core/database/src/db/index.ts
@@ -3,7 +3,9 @@ import * as Layer from "effect/Layer"
 
 import { Database } from "@repo/domain/services/database"
 
-import { DrizzleDb } from "./drizzle.js"
+import { DrizzleDb, DrizzleDbLive } from "./drizzle.js"
+import { MigrateDatabase } from "./migrator.js"
+import { PgPoolClientLive } from "./pool.js"
 import { TryQuery } from "./util.js"
 
 export { PgPoolClient, PgPoolClientLive } from "./pool.js"
@@ -11,7 +13,13 @@ export { PgPoolClient, PgPoolClientLive } from "./pool.js"
 export const DatabaseLive = Layer.effect(
 	Database,
 	Effect.gen(function* () {
-		const db = yield* DrizzleDb
+		{
+			yield* MigrateDatabase
+		}
+
+		const poolClientLive = PgPoolClientLive
+		const drizzleDbLive = DrizzleDbLive.pipe(Layer.provide(poolClientLive))
+		const db = yield* DrizzleDb.asEffect().pipe(Effect.provide(drizzleDbLive))
 
 		return {
 			healthCheck: TryQuery(db.execute("select 1").then(() => true)),
