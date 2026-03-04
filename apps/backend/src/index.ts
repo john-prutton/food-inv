a8e6485611f638858f9f1982263f120b7df4f9a3
John Prutton <johnrprutton@gmail.com>
2026-03-04 23:23:15 +0200

    [Refactor:backend] Cleanup index file

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,5 @@
 import * as Layer from "effect/Layer"
 import * as HttpRouter from "effect/unstable/http/HttpRouter"
-import * as NodeFileSystem from "@effect/platform-node/NodeFileSystem"
 import * as NodeHttpPlatform from "@effect/platform-node/NodeHttpPlatform"
 import * as NodeHttpServer from "@effect/platform-node/NodeHttpServer"
 import * as NodeRuntime from "@effect/platform-node/NodeRuntime"
@@ -8,21 +7,20 @@ import * as NodeRuntime from "@effect/platform-node/NodeRuntime"
 import { createServer } from "node:http"
 
 import { AuthLive } from "@repo/auth/index.js"
+import { DatabaseLive } from "@repo/database"
 
 import { ApiRouter } from "./api/index.js"
 import { StaticFilesRouter } from "./static/index.js"
 
 const AllRouters = Layer.merge(ApiRouter, StaticFilesRouter)
 
-const FileSystem = NodeFileSystem.layer
-
 const HttpServer = NodeHttpServer.layer(createServer, {
 	port: 3001,
 })
 
 const RouterLive = HttpRouter.serve(AllRouters).pipe(
+	Layer.provide(DatabaseLive),
 	Layer.provide(AuthLive),
-	Layer.provide(FileSystem),
 	Layer.provide(HttpServer),
 	Layer.provide(NodeHttpPlatform.layer),
 	Layer.orDie,
