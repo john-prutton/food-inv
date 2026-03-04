cc6b01a4cb01e28e2ea5359ee930839bc5551859
John Prutton <johnrprutton@gmail.com>
2026-03-04 23:05:29 +0200

    [Feature:database] Add auth and user queries

--- core/database/src/db/index.ts
+++ core/database/src/db/index.ts
@@ -1,11 +1,18 @@
 import * as Effect from "effect/Effect"
 import * as Layer from "effect/Layer"
 
+import { eq } from "drizzle-orm"
+
 import { Database } from "@repo/domain/services/database"
 
 import { DrizzleDb, DrizzleDbLive } from "./drizzle.js"
 import { MigrateDatabase } from "./migrator.js"
 import { PgPoolClientLive } from "./pool.js"
+import {
+	oauthAccountsTable,
+	sessionsTable,
+	usersTable,
+} from "./schema/index.js"
 import { TryQuery } from "./util.js"
 
 export { PgPoolClient, PgPoolClientLive } from "./pool.js"
@@ -14,11 +21,46 @@ export const DatabaseLive = Layer.effect(
 	Database,
 	Effect.gen(function* () {
 		yield* MigrateDatabase
+
 		const db = yield* DrizzleDb
 
 		return {
 			healthCheck: () => TryQuery(db.execute("select 1").then(() => true)),
 
+			user: {
+				createUser: (user) =>
+					TryQuery(
+						db
+							.insert(usersTable)
+							.values(user)
+							.returning()
+							.then(([user]) => user!.id),
+					),
+				getUserByEmail: (email) =>
+					TryQuery(
+						db
+							.select()
+							.from(usersTable)
+							.where(eq(usersTable.email, email))
+							.then((results) => results.at(0) ?? null),
+					),
+			},
+			auth: {
+				createSession: (session) =>
+					TryQuery(db.insert(sessionsTable).values(session)),
+
+				recordUserOAuthProvider: (userId, oauthProviderUserId, oauthProvider) =>
+					TryQuery(
+						db
+							.insert(oauthAccountsTable)
+							.values({
+								providerId: oauthProvider,
+								providerUserId: oauthProviderUserId,
+								userId,
+							})
+							.onConflictDoNothing(),
+					),
+			},
 		}
 	}),
 ).pipe(Layer.provide(DrizzleDbLive), Layer.provide(PgPoolClientLive))
