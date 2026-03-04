31bd677348bdeca6c638769b780f150487c779bc
John Prutton <johnrprutton@gmail.com>
2026-03-04 22:35:09 +0200

    [Feature:domain] Add auth and user queries

--- domain/src/services/database/index.ts
+++ domain/src/services/database/index.ts
@@ -1,11 +1,17 @@
-import * as Data from "effect/Data"
 import * as Effect from "effect/Effect"
+import * as Schema from "effect/Schema"
 import * as ServiceMap from "effect/ServiceMap"
 
-export class DatabaseError extends Data.TaggedError("DatabaseError")<{
-	message: string
-	error: string
-}> {}
+import type { OAuthProvider, Session } from "@/schema/auth/index.js"
+import type { Email, User } from "@/schema/user/index.js"
+
+export class DatabaseError extends Schema.TaggedErrorClass<DatabaseError>()(
+	"DatabaseError",
+	{
+		message: Schema.String,
+		error: Schema.String,
+	},
+) {}
 
 type DatabaseQuery<T> = Effect.Effect<T, DatabaseError>
 
@@ -13,5 +19,19 @@ export class Database extends ServiceMap.Service<
 	Database,
 	{
 		readonly healthCheck: DatabaseQuery<true>
+
+		readonly user: {
+			readonly getUserByEmail: (email: Email) => DatabaseQuery<User | null>
+			readonly createUser: (user: Omit<User, "id">) => DatabaseQuery<User["id"]>
+		}
+
+		readonly auth: {
+			readonly createSession: (session: Session) => DatabaseQuery<void>
+			readonly recordUserOAuthProvider: (
+				userId: User["id"],
+				oauthProviderUserId: string,
+				oauthProvider: OAuthProvider,
+			) => DatabaseQuery<void>
+		}
 	}
 >()("Database") {}
