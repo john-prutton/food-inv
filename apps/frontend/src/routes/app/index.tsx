7f4a30e740b0ae0e8257031af586002fa4f6813a
John Prutton <johnrprutton@gmail.com>
2026-03-09 22:24:26 +0200

    [Fix:frontend] Add auth check to layout

--- apps/frontend/src/routes/app/index.tsx
+++ apps/frontend/src/routes/app/index.tsx
@@ -6,11 +6,9 @@ import { SearchBar } from "@/components/dashboard/search-bar"
 import { ExpiringSoon } from "@/components/dashboard/sections/expiring-soon"
 import { StaplesSection } from "@/components/dashboard/sections/staples"
 import { StatCards } from "@/components/dashboard/sections/stat-cards"
-import { CheckAuthOrRedirect } from "@/lib/auth/get-logged-in-user"
 
 export const Route = createFileRoute("/app/")({
 	component: RouteComponent,
-	beforeLoad: CheckAuthOrRedirect,
 })
 
 function RouteComponent() {
