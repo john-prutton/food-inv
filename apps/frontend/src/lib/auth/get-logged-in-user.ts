327ac19861836b4803eead8438b14b08b4324e9c
John Prutton <johnrprutton@gmail.com>
2026-03-11 20:30:45 +0200

    [Refactor:frontend] Use pipe instead of gen

--- apps/frontend/src/lib/auth/get-logged-in-user.ts
+++ apps/frontend/src/lib/auth/get-logged-in-user.ts
@@ -1,21 +1,21 @@
 import * as Effect from "effect/Effect"
+import * as Function from "effect/Function"
 
 import { redirect } from "@tanstack/react-router"
 
 import { ApiClient } from "../api-client"
 
-export const CheckAuthOrRedirect = Effect.fn(
-	function* () {
-		const api = yield* ApiClient
-		return yield* api.auth.me()
-	},
-	Effect.catch(() =>
-		Effect.succeed(
-			redirect({
-				to: "/auth/login",
-				search: { redirect: window.location.toString() },
-			}),
+export const CheckAuthOrRedirect = () =>
+	Function.pipe(
+		ApiClient,
+		Effect.andThen((api) => api.auth.me()),
+		Effect.catch(() =>
+			Effect.succeed(
+				redirect({
+					to: "/auth/login",
+					search: { redirect: window.location.toString() },
+				}),
+			),
 		),
-	),
-	Effect.runPromise,
-)
+		Effect.runPromise,
+	)
